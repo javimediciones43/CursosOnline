2116cd86addd61fa51ce14f29d82c9d5affcfd91
javimediciones <javiero_212@hotmail.com>
2025-06-12 00:22:11 -0300

    Crea controller para gestionar las autenticaciones y las rutaa para el login y logout

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
+// Controller creado para gestionar la autenticacion.
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json(['token' => $token, 'accepted' => true]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        $request->user()->tokens->delete();
+        return response()->json(['message' => 'La sesión se ha cerrado correctamente']);
+    }
+}
